39bb02ac70e97e6593b9b2902efd55e324664536
Dizzy <739091458@qq.com>
2023-03-04 21:52:56 +0800

    Show before unload

--- lib/tool/platform/platform_html.dart
+++ lib/tool/platform/platform_html.dart
@@ -1,6 +1,9 @@
 import 'dart:html';
 import 'dart:js';
-import 'package:xxim_flutter_enterprise/xxim/xxim.dart';
+import 'package:xxim_flutter_enterprise/pages/contact/contact.dart';
+import 'package:xxim_flutter_enterprise/pages/mine/mine.dart';
+import 'package:xxim_flutter_enterprise/pages/news/news.dart';
+import 'package:xxim_flutter_enterprise/main.dart';
 
 class PlatformTool {
   static void webContextMenu() {
@@ -13,6 +16,9 @@ class PlatformTool {
     context.callMethod("addEventListener", [
       "beforeunload",
       (event) {
+        Get.delete<NewsLogic>(force: true);
+        Get.delete<ContactLogic>(force: true);
+        Get.delete<MineLogic>(force: true);
         XXIM.instance.disconnect();
       }
     ]);
