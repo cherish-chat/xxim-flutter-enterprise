3615d7b8981b46ab17d1b773654687a08b7efa80
马守越 <showurl@163.com>
2023-11-13 16:08:28 +0800

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,4 @@
-const String defConfigFile =
-    "https://showurl-test-1259311975.cos.ap-beijing.myqcloud.com/config.amazon.json";
+const String defConfigFile = "";
 const Map defConfigMap = {};
 
 /// config文件解密
@@ -8,7 +7,6 @@ const String encryptAESIV = "tttttteeeeeesssssstttttt";
 
 /// socket通讯公钥
 const String rsaPublicKey = "-----BEGIN PUBLIC KEY-----\n"
-    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuknt6qcc53juo3oWKr6PHBzwgKIp0Ot4rEYecSuHiQ6k5V+nOttdopqAqTvKcTZ9GZAyc+U86AhwR6lfKWcjZMyDgrgery+OkTILjE4orfQIF6zrGqk+ZkXU9ZfjwYkTo7h1a0zGlGsbPZnspisPAgw37vBmIOtW0uBxQUzqSyfSa+LpjC97AO8/YeuceW+K+MCXIG/04PBHJ425GdkQVgzQDtq0KBF3JrBb+z1chW4pH8EpmdZ3uvhmkgrv0EeRy4iH4R6lwms7I2Nuq6ziwu6zCTpYYAAykfDS0ya3tdnx1SX8vdRPy4zrBxu91oDYaQxJVOa0dE+7UKl/QntcpwIDAQAB"
     "\n-----END PUBLIC KEY-----";
 
 /// 控制是否可以发送大于20M的文件
